c2a092e36a9c3e99e1aa65feb620d869a0fb560e
Mike Morris <mike.morris.wa@gmail.com>
2016-09-10 09:31:01 +0800

    Update page title

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const Examples = (props) => {
   return (
     <div>
-      <h3>Examples Component!</h3>
+      <h3>Examples</h3>
       <p>Welcome to the examples page!</p>
     </div>
   );
