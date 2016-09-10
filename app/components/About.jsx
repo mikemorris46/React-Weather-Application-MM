7041a9c2a77bb22c750b19b924aecb8a60ce564f
Mike Morris <mike.morris.wa@gmail.com>
2016-09-10 09:50:41 +0800

    Update About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 
 const About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h3>About</h3>
+      <p>Welcome to the About Page!</p>
+      <div>
+        <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Nulla eget quam vel lacus faucibus dignissim.
+          Duis vel placerat turpis. Duis pellentesque aliquam elit,
+          nec consectetur turpis blandit non. Nullam suscipit a ante eget porttitor.
+          Sed accumsan faucibus justo non mollis. Nulla nec rutrum tortor.
+          Aenean at magna ac nisl posuere vestibulum ut ac ex.
+          Vestibulum non tortor euismod, malesuada lacus vel, accumsan ipsum.
+          Ut egestas varius enim cursus maximus. Integer convallis et est at dictum.
+        </blockquote>
+      </div>
+    </div>
   );
 }
 
